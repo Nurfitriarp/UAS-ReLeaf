6c26e4bba8feaeaa4b4a1947919952838be56132
Nur fitria Rahmadani putri <nurfitriarput35@gmail.com>
2025-12-25 14:10:08 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8200126187-cc4e2",
+  "appId": "1:731769034632:web:0c7bd1617b34e60b9d4c3d",
+  "apiKey": "AIzaSyArE2JuyMkg8mvOJqE2Q0wKVDfSnzctBlQ",
+  "authDomain": "studio-8200126187-cc4e2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "731769034632"
+};
